5c52c872812918680129504ae4be4d2ff6114903
pongsak-pak <76504766+pongsak-pak@users.noreply.github.com>
2021-03-13 00:47:25 +0700

    Add files via upload

---
+++ insert_from.php
@@ -0,0 +1,11 @@
+  
+<html>
+    <body>
+    <from action="insert.php" method="post">
+        ชื่อ:<input type="text" name="firstname" /> <br/>
+        นามสกุล:<input type="text" name="lastname" /> <br/>
+        email:<input type="text" value="@gmail.com" name="email" /> <br/>
+        <input type="submit" value="บันทึก" name="submit_btn" />
+    </from>
+    </body>
+</html>
